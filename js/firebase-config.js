118c6f026ef0f8b87aa2ff1df9d5f3d29437bb4d
Bruce Zhao <97787652+BruceZ-hao@users.noreply.github.com>
2026-02-14 12:02:30 +0800

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,22 +1,24 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyCL7qHx-kmj7PDhfnm_AcwKoMCZtw7xeFs",
-  authDomain: "typequest-leaderboard.firebaseapp.com",
-  databaseURL: "https://typequest-leaderboard-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "typequest-leaderboard",
-  storageBucket: "typequest-leaderboard.firebasestorage.app",
-  messagingSenderId: "57783452163",
-  appId: "1:57783452163:web:dbab039b3f64198ce80436",
-  measurementId: "G-L2J5QX000S"
-};
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyCL7qHx-kmj7PDhfnm_AcwKoMCZtw7xeFs",
+    authDomain: "typequest-leaderboard.firebaseapp.com",
+    databaseURL: "https://typequest-leaderboard-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "typequest-leaderboard",
+    storageBucket: "typequest-leaderboard.firebasestorage.app",
+    messagingSenderId: "57783452163",
+    appId: "1:57783452163:web:dbab039b3f64198ce80436",
+    measurementId: "G-L2J5QX000S"
+  };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+</script>
